e62eaaa24fd5f2befbfa436ffe45cfacb62eba23
Rojan Aryal <rojanaryal@Rojans-MacBook-Air.local>
2023-11-01 20:56:15 +0545

    Final change Nov1

---
+++ Nov 1/script.js
@@ -0,0 +1,32 @@
+var x=5;
+console.log(x);
+
+const food='Sausage'
+console.log(food)
+
+let number= 7;
+console.log(number)
+
+// Reads from top to bottom
+var beverage='Pepsi';
+console.log(beverage)
+var beverage='Coke';
+console.log(beverage)
+
+const a='Pepsi';
+console.log(a)
+// const a='Coke';
+// console.log(a);
+
+
+let tarkari='aloo';
+console.log(tarkari);
+  tarkari='Mula';
+console.log(tarkari)
+
+// Hoisting
+var x=9;
+console.log(x)
+
+// var x (Declare)
+// x=9 (Initialization)
